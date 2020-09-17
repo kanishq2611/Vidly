1ac1a4472fad41cb85210ab448987ca2aa30ce0f
kanishq <kanishqagrawal2611@gmail.com>
2020-09-17 23:58:43 +0530

    added new index file for the template folder root

--- vidly/index.js
+++ vidly/index.js
@@ -1,10 +1,14 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost/vidly')
+	.then(()=> console.log('Connection to MongoDB successful'))
+	.catch(err=> console.error('Could not connect...'));
 app.use(express.json());
-app.use('api/genres',genres);
-
+app.use('/api/genres/',genres);
+app.use('/api/customers/',customers)
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
